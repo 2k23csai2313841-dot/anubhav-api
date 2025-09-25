d42ab9cf7e70082f26decf296cd9da8a5bb7e937
2k23csai2313841-dot <2k23.csai2313841@gmail.com>
2025-09-26 00:17:32 +0530

    Update api.js

--- api.js
+++ api.js
@@ -29,12 +29,13 @@ app.post("/send-email", async (req, res) => {
   service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    pass: process.env.EMAIL_PASS
   },
-  tls: {
-    rejectUnauthorized: false
-  }
+  tls: { rejectUnauthorized: false },
+  logger: true,
+  debug: true
 });
+
 
 
 
@@ -57,6 +58,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`✅ Anubhav Mail API running on port ${PORT}`)
 );
+
 
 
 
