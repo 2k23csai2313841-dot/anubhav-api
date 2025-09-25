064b78349e11304efae4973796b221544ce476ef
2k23csai2313841-dot <2k23.csai2313841@gmail.com>
2025-09-25 23:17:02 +0530

    Update api.js

--- api.js
+++ api.js
@@ -9,12 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-res.send("Anubhav Api");
-});
-app.get("/send-email",(req,res)=>{
-res.send("Anubhav Api");
-});
+
 // API endpoint
 app.post("/send-email", async (req, res) => {
   const { to, subject, message } = req.body;
@@ -50,5 +45,6 @@ app.post("/send-email", async (req, res) => {
 app.listen(5000, () =>
   console.log("✅ Mail API running on http://localhost:5000")
 );
+
 
 
