c88d36f82fb0c5515e7a80f2164aea7baef51b8a
2k23csai2313841-dot <2k23.csai2313841@gmail.com>
2025-09-26 00:03:54 +0530

    Update api.js

--- api.js
+++ api.js
@@ -25,15 +25,17 @@ app.post("/send-email", async (req, res) => {
   }
 
   try {
-    const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+   const transporter = nodemailer.createTransport({
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false
+  }
 });
+
 
 
     await transporter.sendMail({
@@ -55,6 +57,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`✅ Anubhav Mail API running on port ${PORT}`)
 );
+
 
 
 
